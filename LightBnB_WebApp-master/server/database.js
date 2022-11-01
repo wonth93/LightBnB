bfcab9a795db034b322b60314407179c65ec54d9
wonth93 <wongth93@gmail.com>
2022-11-01 05:57:11 +0000

    set up javascript project

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'bootcampx'
+// });
+
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)});
 
 /// Users
 
